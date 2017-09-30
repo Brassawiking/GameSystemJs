cd17d20df4a01922c1231ee5800f51c12633c423
Johan Ludvigsson <brassawiking@gmail.com>
2017-09-30 17:16:37 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.registerElement('game-system', {
+  prototype: Object.create(HTMLElement.prototype, {
+    createdCallback: { value: function () { return created(this); } }
+  })
+})
+
+function created(element) {
+  element.innerHTML = 'GAME SYSTEM JS'
+}
