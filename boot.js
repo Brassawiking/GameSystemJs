ca5cebb63522da6f91325e7292f494492ef9f63d
Johan Ludvigsson <brassawiking@gmail.com>
2017-11-29 13:00:24 +0100

    Logo is now correctly displayed

--- boot.js
+++ boot.js
@@ -10,19 +10,20 @@ m[BGP]=0xfc
 m[SCX] = 0;
 m[SCY] = 0;
 
+//for(i=0;i<1024;++i)m[BGMAP1+i]=i%256
 
 
 
 var z = 2;
 for(i=0;i<2;++i) {
-  for(j=0;j<6;++j) {
-    var x = 7+j;
-    var y = 7+i;
+  for(j=0;j<12;++j) {
+    var x = 4+j;
+    var y = 8+i;
     m[BGMAP1 + x + y*32] = z++
   }
 }
 
-m[BGMAP1 + 13 + 7*32] = 1
+m[BGMAP1 + 16 + 8*32] = 1
 
 
 m[SCX] = 0;
@@ -31,23 +32,21 @@ var c = 0;
 var dt = 0;
 
 m[HSYNC] = function (y, height) {
-  c = Math.sin((2*y/height) + (dt / 20)) * 40;
+  //c = Math.sin((2*y/height) + (dt / 20)) * 40;
 
-  m[SCX] = y % 2 ? c : c;
-  m[SCY] = c;
+  //m[SCX] = y % 2 ? c : c;
+  //m[SCY] = c;
 }
 
 
 m[VSYNC] = function () {
-  dt++;
+  //dt++;
   
   //m[SCY]--;
 }
 
 dma(CHAR+(16*1), scaleUpR(l2))
 dma(CHAR+(16*2), scaleUpLogo(l1))
-
-debug(l1.length)
 
 function scaleUpR(bytes){
   var result = [];
@@ -78,7 +77,7 @@ function scaleUpR(bytes){
 
 function scaleUpLogo(bytes){
   var result = [];
-  for (var i=0; i < bytes.length; i += 4) {
+  for (var i=0; i < bytes.length; i += 2) {
     var b = bytes[i];
 
     var x8 = (b >> 7) & 1
@@ -91,11 +90,12 @@ function scaleUpLogo(bytes){
     var x2 = (b >> 1) & 1
     var x1 = (b >> 0) & 1
     
-    var b1 = (x8 << 7) | (x8 << 6) | (x7 << 5) | (x7 << 4) |(x6 << 3) |(x6 << 2) |(x5 << 1) | (x5 << 0);
-    var b2 = (x4 << 7) | (x4 << 6) | (x3 << 5) | (x3 << 4) |(x2 << 3) |(x2 << 2) |(x1 << 1) | (x1 << 0);
+    var b1 = (x8 << 7) | (x8 << 6) | (x8 << 5) | (x8 << 4) |(x7 << 3) |(x7 << 2) |(x7 << 1) | (x7 << 0);
+    var b2 = (x6 << 7) | (x6 << 6) | (x6 << 5) | (x6 << 4) |(x5 << 3) |(x5 << 2) |(x5 << 1) | (x5 << 0);
+    var b3 = (x4 << 7) | (x4 << 6) | (x4 << 5) | (x4 << 4) |(x3 << 3) |(x3 << 2) |(x3 << 1) | (x3 << 0);
+    var b4 = (x2 << 7) | (x2 << 6) | (x2 << 5) | (x2 << 4) |(x1 << 3) |(x1 << 2) |(x1 << 1) | (x1 << 0);
    
-
-
+   
     var b = bytes[i+1];
 
     var x8 = (b >> 7) & 1
@@ -108,62 +108,32 @@ function scaleUpLogo(bytes){
     var x2 = (b >> 1) & 1
     var x1 = (b >> 0) & 1
     
-    var b3 = (x8 << 7) | (x8 << 6) | (x7 << 5) | (x7 << 4) |(x6 << 3) |(x6 << 2) |(x5 << 1) | (x5 << 0);
-    var b4 = (x4 << 7) | (x4 << 6) | (x3 << 5) | (x3 << 4) |(x2 << 3) |(x2 << 2) |(x1 << 1) | (x1 << 0);
+    var b5 = (x8 << 7) | (x8 << 6) | (x8 << 5) | (x8 << 4) |(x7 << 3) |(x7 << 2) |(x7 << 1) | (x7 << 0);
+    var b6 = (x6 << 7) | (x6 << 6) | (x6 << 5) | (x6 << 4) |(x5 << 3) |(x5 << 2) |(x5 << 1) | (x5 << 0);
+    var b7 = (x4 << 7) | (x4 << 6) | (x4 << 5) | (x4 << 4) |(x3 << 3) |(x3 << 2) |(x3 << 1) | (x3 << 0);
+    var b8 = (x2 << 7) | (x2 << 6) | (x2 << 5) | (x2 << 4) |(x1 << 3) |(x1 << 2) |(x1 << 1) | (x1 << 0);
 
 
-    var b = bytes[i+2];
-
-    var x8 = (b >> 7) & 1
-    var x7 = (b >> 6) & 1
-    var x6 = (b >> 5) & 1
-    var x5 = (b >> 4) & 1
-    
-    var x4 = (b >> 3) & 1
-    var x3 = (b >> 2) & 1
-    var x2 = (b >> 1) & 1
-    var x1 = (b >> 0) & 1
-    
-    var b5 = (x8 << 7) | (x8 << 6) | (x7 << 5) | (x7 << 4) |(x6 << 3) |(x6 << 2) |(x5 << 1) | (x5 << 0);
-    var b6 = (x4 << 7) | (x4 << 6) | (x3 << 5) | (x3 << 4) |(x2 << 3) |(x2 << 2) |(x1 << 1) | (x1 << 0);
 
    
-   
-    var b = bytes[i+3];
-
-    var x8 = (b >> 7) & 1
-    var x7 = (b >> 6) & 1
-    var x6 = (b >> 5) & 1
-    var x5 = (b >> 4) & 1
-    
-    var x4 = (b >> 3) & 1
-    var x3 = (b >> 2) & 1
-    var x2 = (b >> 1) & 1
-    var x1 = (b >> 0) & 1
-    
-    var b7 = (x8 << 7) | (x8 << 6) | (x7 << 5) | (x7 << 4) |(x6 << 3) |(x6 << 2) |(x5 << 1) | (x5 << 0);
-    var b8 = (x4 << 7) | (x4 << 6) | (x3 << 5) | (x3 << 4) |(x2 << 3) |(x2 << 2) |(x1 << 1) | (x1 << 0);
-   
-   
-    
     result.push(b1);
-    result.push(b5);
+    result.push(b2);
     result.push(b1);
-    result.push(b5);
-
     result.push(b2);
-    result.push(b6);
-    result.push(b2);
-    result.push(b6);
 
     result.push(b3);
-    result.push(b7);
-    result.push(b3);
-    result.push(b7);
-
     result.push(b4);
+    result.push(b3);
+    result.push(b4);
+
+    result.push(b5);
+    result.push(b6);
+    result.push(b5);
+    result.push(b6);
+
+    result.push(b7);
     result.push(b8);
-    result.push(b4);
+    result.push(b7);
     result.push(b8);
 }
 
